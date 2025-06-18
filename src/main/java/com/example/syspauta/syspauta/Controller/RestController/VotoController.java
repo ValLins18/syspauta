445cda915a41e3c919ad5e971b7eecafe44aeeec
Valdemilson Junior <valdemilson.lins@gmail.com>
2025-06-18 14:40:12 -0300

    Implementando Integração com API terceira que verifica se cpf pode ou nao votar

--- src/main/java/com/example/syspauta/syspauta/Controller/RestController/VotoController.java
+++ src/main/java/com/example/syspauta/syspauta/Controller/RestController/VotoController.java
@@ -3,12 +3,14 @@ package com.example.syspauta.syspauta.Controller.RestController;
 import com.example.syspauta.syspauta.Model.Voto;
 import com.example.syspauta.syspauta.Repository.SessaoRepository;
 import com.example.syspauta.syspauta.Repository.VotoRepository;
+import com.example.syspauta.syspauta.Service.ValidadorCpfService;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/voto")
@@ -17,19 +19,26 @@ public class VotoController {
     private final SessaoRepository sessaoRepository;
     private final VotoRepository votoRepository;
     private final Environment env;
+    private final ValidadorCpfService validadorCpfService;
 
-    public VotoController(SessaoRepository sessaoRepository, VotoRepository votoRepository, Environment env) {
+    public VotoController(SessaoRepository sessaoRepository, VotoRepository votoRepository, Environment env, ValidadorCpfService validadorCpfService) {
         this.sessaoRepository = sessaoRepository;
         this.votoRepository = votoRepository;
         this.env = env;
+        this.validadorCpfService = validadorCpfService;
     }
 
     @PostMapping("/votar/{sessaoId}")
-    public ResponseEntity<Voto> votar(@RequestBody Voto voto, @PathVariable Long sessaoId) {
+    public ResponseEntity<?> votar(@RequestBody Voto voto, @PathVariable Long sessaoId) {
 
         //Verifica se o associado ja votou naquela pauta e sessão
         if(votoRepository.existsByCpfAssociadoAndSessaoId(voto.getCpfAssociado(), sessaoId)){
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(Map.of("erro", "O associado com este cpf ja votou nesta pauta!!"));
+        }
+
+        //Verifica em uma API externa se o associado está apto para votar
+        if(!validadorCpfService.podeVotar(voto.getCpfAssociado())) {
+            return ResponseEntity.notFound().build();
         }
 
         var sessao = sessaoRepository.findById(sessaoId).orElseThrow(() -> new RuntimeException("Sessao não encontrada para fazer o voto"));
---
+++ src/main/java/com/example/syspauta/syspauta/Service/ValidadorCpfService.java
@@ -0,0 +1,28 @@
+package com.example.syspauta.syspauta.Service;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+
+@Service
+public class ValidadorCpfService {
+
+    private final RestTemplate restTemplate;
+    private final Environment environment;
+
+    public ValidadorCpfService(RestTemplate restTemplate, Environment environment) {
+        this.restTemplate = restTemplate;
+        this.environment = environment;
+    }
+
+    public Boolean podeVotar(String cpf) {
+        String url = String.format(environment.getProperty("url.valida.cpf"), cpf);
+
+        var validationResponse = restTemplate.getForObject(url, Map.class);
+
+        return validationResponse != null && "ABLE_TO_VOTE".equals(validationResponse.get("status"));
+    }
+}
