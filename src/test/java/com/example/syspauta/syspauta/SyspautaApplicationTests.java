bdc9c8200ef3c60420b59c6efef6dd3c9c0ce17c
Valdemilson Junior <valdemilson.lins@gmail.com>
2025-06-18 01:40:52 -0300

    Commit Inicial

---
+++ src/test/java/com/example/syspauta/syspauta/SyspautaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syspauta.syspauta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyspautaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
